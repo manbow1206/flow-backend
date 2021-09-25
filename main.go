264b18e677f8e1559a4db8a6ca184b00cc959012
manbow1206 <tyasunaga@iyell.jp>
2021-09-25 16:18:59 +0900

    [add]変数を使用

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
-// main宣言、main()から実行
 package main
 
-// パッケージを宣言
 import (
-    f "fmt" // 別名
-    // _ "string" // パッケージを使用しない
-    // "exsample"
+	f "fmt" // 別名
 )
 
+// var message string = "変数を使用"
+
 func main() {
-    f.Println("まんぼう")
+
+	// var message string = "変数を使用"
+
+	// 複数変数宣言
+	// var foo, bar, buz = "foo", "bar", "buz"
+	var (
+		foo = "foo"
+		bar = "bar"
+		buz = "buz"
+	)
+	f.Println(foo)
+	f.Println(bar)
+	f.Println(buz)
+
+	// 省略変数
+	message := "略変数を使用"
+	f.Println(message)
+
+	// 定数
+	const Constant string = "定数を使用"
+	f.Println(Constant)
+
 }
