54084e469927cff57c6ee34d55c1a2429b9ec1a4
manbow1206 <tyasunaga@iyell.jp>
2021-09-26 12:14:53 +0900

    [add]format

--- main.go
+++ main.go
@@ -163,100 +163,97 @@ func main() {
 	// }
 
 	// returns Ver
-    // 戻り値の変数を個数分用意
+	// 戻り値の変数を個数分用意
 	x, y := 3, 4
 	x, y = RetunrsFunc(x, y)
-    f.Println(x, y)
+	f.Println(x, y)
 
-    // NG 戻り値の変数を個数分用意していない
-    // cannot assign 2 values to 1 variables
+	// NG 戻り値の変数を個数分用意していない
+	// cannot assign 2 values to 1 variables
 	// x = RetunrsFunc(x, y)
 
+	// 必要な戻り値のみ用意
+	x, _ = RetunrsFunc(x, y)
+	f.Println(x)
 
-    // 必要な戻り値のみ用意
-    x, _ = RetunrsFunc(x, y)
-    f.Println(x)
+	// エラーを返り値に設定
+	// エラー値は最後にするのが慣習、自作エラーパッケージを作成し、それを使用する事も可能
+	// file, err := os.Open("hello world")
+	// if err != nil {
+	//     // エラー処理
+	// }
+	// fileを用いた処理
 
+	// 名前付き戻り値
+	// メリット : 関数の宣言時から戻り値の意味が把握できる。return時の書き間違いなど防止
+	// func NamaReturn(i,j int) (result int, err error) {
+	//     if j == 0 {
+	//         err = errors.Ner("divied by zero")
+	//         return //自動的に名前付き返り値が返される。この場合はreturn 0, err
+	//     }
+	//     result = i / j
+	//     return //return result, nil と同じ
+	// }
 
-    // エラーを返り値に設定
-    // エラー値は最後にするのが慣習、自作エラーパッケージを作成し、それを使用する事も可能
-    // file, err := os.Open("hello world")
-    // if err != nil {
-    //     // エラー処理
-    // }
-    // fileを用いた処理
+	// 関数リテラル
+	// その場で使用する無名関数
+	func(i, j int) {
+		f.Println(i + j)
+	}(2, 4)
 
-    // 名前付き戻り値
-    // メリット : 関数の宣言時から戻り値の意味が把握できる。return時の書き間違いなど防止
-    // func NamaReturn(i,j int) (result int, err error) {
-    //     if j == 0 {
-    //         err = errors.Ner("divied by zero")
-    //         return //自動的に名前付き返り値が返される。この場合はreturn 0, err
-    //     }
-    //     result = i / j
-    //     return //return result, nil と同じ
-    // }
+	// 変数に関数リテラルを入れる場合
+	var sum func(i, j int) = func(i, j int) {
+		f.Println(i + j)
+	}
 
-    // 関数リテラル
-    // その場で使用する無名関数
-    func(i, j int) {
-        f.Println(i + j)
-    }(2, 4)
+	sum(2, 4)
 
-    // 変数に関数リテラルを入れる場合
-    var sum func(i ,j int) = func(i, j int){
-        f.Println(i + j)
-    }
-
-    sum(2,4)
-
-    // 配列 固定長
+	// 配列 固定長
 	f.Println("---Array syntax---")
-    var arr [4]string
-    arr[0] = "マンボウ"
-    arr[1] = "ジンベイザメ"
-    arr[2] = "イルカ"
+	var arr [4]string
+	arr[0] = "マンボウ"
+	arr[1] = "ジンベイザメ"
+	arr[2] = "イルカ"
 
-    f.Println(arr)
-    f.Println(arr[1])
-    f.Println(arr[2])
+	f.Println(arr)
+	f.Println(arr[1])
+	f.Println(arr[2])
 
-
-    // 宣言と同時に配列長を暗黙的に指定
-    arr1 := [4]string{"マンボウ", "ジンベイザメ","いるか","まぐろ"}
-    arr2 := [...]string{"マンボウ", "ジンベイザメ","いるか","まぐろ"}
+	// 宣言と同時に配列長を暗黙的に指定
+	arr1 := [4]string{"マンボウ", "ジンベイザメ", "いるか", "まぐろ"}
+	arr2 := [...]string{"マンボウ", "ジンベイザメ", "いるか", "まぐろ"}
 
 	f.Println("配列長、直接指定")
-    f.Println(arr1)
+	f.Println(arr1)
 	f.Println("配列長、暗黙指定")
-    f.Println(arr2)
+	f.Println(arr2)
 
-    // 配列長も情報として含む
-    // func CheckArrayLength(arr [4]string) {
-    //     f.Printlna(arr)
-    // }
-    // var arr3 [4]string
-    // var arr4 [4]string
-    // CheckArrayLength(arr3) //ok
-    // CheckArrayLength(arr4) //NG 長さが違う為
+	// 配列長も情報として含む
+	// func CheckArrayLength(arr [4]string) {
+	//     f.Printlna(arr)
+	// }
+	// var arr3 [4]string
+	// var arr4 [4]string
+	// CheckArrayLength(arr3) //ok
+	// CheckArrayLength(arr4) //NG 長さが違う為
 
-    // 配列が関数に渡された場合、配列はコピーされて、関数内で使用
-    // func fn(arr [4]string) {
-    //     arr[0] = "x"
-    //     fmt.Println(arr) // [x b c d]
-    // }
+	// 配列が関数に渡された場合、配列はコピーされて、関数内で使用
+	// func fn(arr [4]string) {
+	//     arr[0] = "x"
+	//     fmt.Println(arr) // [x b c d]
+	// }
 
-    // func main() {
-    //     arr := [4]string{"a", "b", "c", "d"}
-    //     fn(arr)
-    //     fmt.Println(arr) // [a b c d]
-    // }
+	// func main() {
+	//     arr := [4]string{"a", "b", "c", "d"}
+	//     fn(arr)
+	//     fmt.Println(arr) // [a b c d]
+	// }
 
 }
 
 // returns Ver
 func RetunrsFunc(i, j int) (int, int) {
 	i = i + 1
-    j = j + 1
-    return i, j
+	j = j + 1
+	return i, j
 }
