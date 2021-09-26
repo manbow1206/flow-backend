bacdf1220ff1d7254034e22b669f171e353a9b04
manbow1206 <tyasunaga@iyell.jp>
2021-09-26 12:14:32 +0900

    [add]function 7 関数に対しての配列

--- main.go
+++ main.go
@@ -240,7 +240,17 @@ func main() {
     // CheckArrayLength(arr3) //ok
     // CheckArrayLength(arr4) //NG 長さが違う為
 
+    // 配列が関数に渡された場合、配列はコピーされて、関数内で使用
+    // func fn(arr [4]string) {
+    //     arr[0] = "x"
+    //     fmt.Println(arr) // [x b c d]
+    // }
 
+    // func main() {
+    //     arr := [4]string{"a", "b", "c", "d"}
+    //     fn(arr)
+    //     fmt.Println(arr) // [a b c d]
+    // }
 
 }
 
