87d033494f4ed1b13c1365b37e1b4d96540f484e
manbow1206 <tyasunaga@iyell.jp>
2021-10-05 22:33:30 +0900

    [add]gin file upload

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"io"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
-	// ua := ""
+	engine.POST("/upload", func(c *gin.Context) {
+		file, header, err := c.Request.FormFile("image")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			return
+		}
 
-	// ミドルウェアを使用
-	// engine.Use(func(c *gin.Context) {
-	// 	ua = c.GetHeader("User-Agent")
-	// 	c.Next()
-	// })
-	engine.LoadHTMLGlob("templates/*")
-	engine.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"message": "Hello gin",
+		fileName := header.Filename
+		dir,  _ := os.Getwd()
+		out, err := os.Create(dir + "\\images\\"+fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		 _, err = io.Copy(out, file)
+		 if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
 		})
 	})
 	engine.Run(":10000")
