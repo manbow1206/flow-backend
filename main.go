605155fef9f347e1a2a8985cb78b78d3d3c8d58c
manbow1206 <tyasunaga@iyell.jp>
2021-10-05 21:53:24 +0900

    [add]gin middleware

--- main.go
+++ main.go
@@ -7,9 +7,17 @@ import (
 
 func main() {
 	engine := gin.Default()
+	ua := ""
+
+	// ミドルウェアを使用
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello world",
+			"User-Agent": ua,
 		})
 	})
 	engine.Run(":10000")
